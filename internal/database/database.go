54371a7eb641a1d45d90ca808a855b01cec30f0b
masaya-osuga <msyaaa456910@gmail.com>
2025-12-09 17:38:33 +0900

    Cloud SQLにつなぐ処理を実装した

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,26 +1,65 @@
 package database
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
+	"net"
+	"os"
 
-	"github.com/fun-dotto/announcement-api/internal/config"
+	"cloud.google.com/go/cloudsqlconn"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func Connect(cfg config.DatabaseConfig) (*gorm.DB, error) {
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode,
-	)
-
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to database: %w", err)
+func ConnectWithConnectorIAMAuthN() (*gorm.DB, error) {
+	mustGetenv := func(k string) string {
+		v := os.Getenv(k)
+		if v == "" {
+			log.Fatalf("Warning: %s environment variable not set.", k)
+		}
+		return v
 	}
 
-	log.Printf("Connected to database: %s@%s:%s/%s", cfg.User, cfg.Host, cfg.Port, cfg.DBName)
+	var (
+		dbUser                 = mustGetenv("DB_IAM_USER")              // e.g. 'service-account-name@project-id.iam'
+		dbName                 = mustGetenv("DB_NAME")                  // e.g. 'my-database'
+		instanceConnectionName = mustGetenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
+	)
+
+	d, err := cloudsqlconn.NewDialer(
+		context.Background(),
+		cloudsqlconn.WithIAMAuthN(),
+		cloudsqlconn.WithLazyRefresh(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("cloudsqlconn.NewDialer: %w", err)
+	}
+	var opts []cloudsqlconn.DialOption
+	dsn := fmt.Sprintf("user=%s database=%s", dbUser, dbName)
+	config, err := pgx.ParseConfig(dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	config.DialFunc = func(ctx context.Context, network, instance string) (net.Conn, error) {
+		return d.Dial(ctx, instanceConnectionName, opts...)
+	}
+	dbURI := stdlib.RegisterConnConfig(config)
+	sqlDB, err := sql.Open("pgx", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %w", err)
+	}
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("gorm.Open: %w", err)
+	}
 	return db, nil
 }
 
